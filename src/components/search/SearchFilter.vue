c26b66cab2bbb7833b3d49d65015d445f1d4a305
Igor Kostadinoski <kostadinoski96@gmail.com>
2021-04-06 00:12:15 +0200

    add search filter

---
+++ src/components/search/SearchFilter.vue
@@ -0,0 +1,42 @@
+<template>
+	<div class="columns is-multiline is-mobile" style="padding-top:30px">
+			<div class="column is-one-quarter">
+				<div class="field">
+					<label class="label">Search Products</label>
+          <div class="control">
+                <form>
+                  <input type="search" @input="search" :value="searchTerm" placeholder="Filter items" />
+                </form>
+           </div>
+       </div>
+    </div>
+ </div>
+</template>
+
+<script>
+export default {
+  props: ['searchTerm'],
+  emits: ['search'],
+  methods: {
+    search(event) {
+      this.$emit('search', event.target.value);
+    },
+  },
+};
+</script>
+
+<style scoped>
+input {
+  font: inherit;
+  width: 100%;
+  display: block;
+  padding: 0.15rem;
+  border: 1px solid #ccc;
+}
+
+input:focus {
+  outline: none;
+  border-color: #00006b;
+  background-color: #eeeeff;
+}
+</style>
--- src/pages/ProductsList.vue
+++ src/pages/ProductsList.vue
@@ -1,4 +1,6 @@
 <template>
+    <search-filter @search="updateSearch" :search-term="enteredSearch"></search-filter>
+
 		<div class="columns is-multiline is-mobile" style="padding-top:30px">
          <div class="column is-one-quarter" v-bind:key="prod.id" v-for="prod in products">
       <product-item
@@ -14,15 +16,45 @@
 
 <script>
 import ProductItem from '../components/products/ProductItem.vue';
+import SearchFilter from '../components/search/SearchFilter.vue'
 
 export default {
+  data(){
+    return{
+      enteredSearch: '',
+      activeSearch:'',
+    };
+  },
   computed:{
   products() {
-      return this.$store.getters['prods/getProducts'];
+     
+       return this.$store.getters['prods/getProducts'].filter(element => {
+             if(this.activeSearch !== ''){
+                return(element.title.toLowerCase().match(this.activeSearch.toLowerCase()))
+             } else {
+               return this.$store.getters['prods/getProducts'];
+             }
+        });
     },
+  },
+  methods: {
+    updateSearch(val){
+      return this.enteredSearch = val;
+    }
+  },
+
+   watch: {
+    enteredSearch(val) {
+      setTimeout(() => {
+        if (val === this.enteredSearch) {
+          this.activeSearch = val;
+        }
+      }, 300);
+    }
   },
   components: {
     ProductItem,
+    SearchFilter
   },
 };
 </script>
